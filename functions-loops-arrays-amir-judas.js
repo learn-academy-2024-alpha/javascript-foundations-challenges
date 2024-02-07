f14a00f875835c3547f87722c13d35253ed51525
MFDD19D <judas.sanchez1@yahoo.com>
2024-02-07 09:28:36 -0800

    update fla

---
+++ functions-loops-arrays-amir-judas.js
@@ -0,0 +1,83 @@
+// // 💻 Challenges
+// // Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
+
+// // Don't forget to pseudo code.
+
+// // Write a function that takes in an array of numbers and returns an array with all numbers multiplied by 3.
+
+// // const testArr1 = [3, 9, 15, 4, 10]
+
+// // const multi3 = (arrayOfNums) => { 
+// //  let newArr= []
+// //  for (let i = 0; i < arrayOfNums.length; i++) {
+// //     newArr.push(arrayOfNums[i] * 3)
+// //  }
+// //  return newArr
+// // } 
+// // console.log(multi3(testArr1))
+// // output: [9, 27, 45, 12, 30]
+
+// // Write a function that takes in an array of numbers and returns a new array with only odd numbers.
+
+// // const testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
+// // // this holds our numbers that we will test against
+// // const onlyOdd = (arrayOfNums) => {
+// // //  were creating a variable declaraiton
+// //   let oddsArray = []
+// //     // declaring new name to test our variables
+// //   for (let i = 0; i < arrayOfNums.length; i++) {
+// //     // function to inspect what is in the index
+// //     if (arrayOfNums[i] % 2 !== 0) {
+// //         // if the index number is odd it will extract it out
+// //       oddsArray.push(arrayOfNums[i])
+// //     }
+// //   }
+
+// //   return oddsArray
+// // }
+
+// // console.log(onlyOdd(testArr2))
+// // // output: [-7, 3, 5, 13]
+
+
+// // // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: use the typeof method.
+
+// // const comboArr = [7, "n", true, "i", "c", 10, "e", -388, "w", 3, "o", 0, "r", false, "k"];
+
+// // const onlyLett = (arr) => {
+// //   let result = "";
+
+// //   for (let i = 0; i < arr.length; i++) {
+// //     if (typeof arr[i] === "string") {
+// //       result = result.concat(arr[i])
+// //     }
+// //   }
+// //     return result
+// // }
+
+// // console.log(onlyLett(comboArr)); 
+// // // Output: "nicework"
+
+// // Create a function that takes in an array of numbers and returns the sum.
+// const addThese1 = [1, 2, 3, 4]
+
+// // output: [5, 25, 35, 15, 50]
+
+
+// const numAdd = (array) => {
+//     let newArr = []
+//     for (let i = 0; i< array.length; i++) {
+//     newArr.sumRight(array[i]); 
+//     }Number.prototype.myadd = function
+//     return newArr
+// }
+
+// console.log(numAdd(addThese1));
+
+// // output: 10
+
+// // const addThese2 = []
+// // // output: 0
+// // Create a function that takes in an array of numbers and returns the index of the largest number.
+// // const indexHighestNumber = [1, 4, 2, 3]
+// // // output: 1
