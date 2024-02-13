93fe276191221fa0d1ab35fca64dad3fb18a7bb5
mark19242 <mark19242@gmail.com>
2024-02-13 14:50:51 -0800

    on to stretch goals

--- objects-morgan-mark-shawn.js
+++ objects-morgan-mark-shawn.js
@@ -3,19 +3,21 @@
 const person = {
   firstName: "Arthur",
   lastName: "Dent",
-  homePlanet: "Earth",
+  
   personString: function () {
     return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}`
   }
 }
 // Write the code that accesses the first name of the person object.
-console.log(person.firstName)
+// console.log(person.firstName)
 // Write the code that accesses the last name of the person object.
-console.log(person.lastName)
+// console.log(person.lastName)
 // Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
+person.homePlanet = "Earth"
+console.log(person)
 
 // Update the person object with a method that logs "Arthur Dent is from planet Earth".
-console.log(person.personString())
+// console.log(person.personString())
 // Consider this variable:
 
 const product = {
@@ -31,18 +33,31 @@ console.log(describeProduct(product))
 const totalWithTax = (prodObj) => {
     return (prodObj.price * .07 + prodObj.price).toFixed(2)
 }
-console.log(totalWithTax(product))
+// console.log(totalWithTax(product))
 // Consider this variable:
 
 const lunch = {
   name: "PB and Banana",
   type: "sandwich",
-  ingredients: ["bread", "peanut butter", "banana"]
+  ingredients: ["bread", "peanut butter", "banana"],
+  descibeLunch: function () {
+    return(`The ingredients for a  ${lunch.name} sandwich are ${lunch.ingredients}.`)
+  }
 }
 // Write the code that accesses the ingredients property.
+console.log(lunch.ingredients)
 // Write the code that access the third ingredient of the lunch object.
+console.log(lunch.ingredients[2])
 // Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+const descibeLunch = (lunch) =>{
+  return `The ingredients for a  ${lunch.name} sandwich are ${lunch.ingredients}.`
+}
+
+console.log(descibeLunch(lunch))
+
 // Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+console.log(lunch.descibeLunch())
+
 // Consider this variable:
 
 const animals = [
@@ -52,9 +67,21 @@ const animals = [
   { name: "Hank", type: "cat", age: 11 }
 ]
 // Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+// const justCats = (animals) => animals.filter(animal => animal.type === "cat");
+// console.log(justCats(animals))
+
 // Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
+// const animalNames = (animals) => animals.map(animal => animal.name);
+// console.log(animalNames(animals))
+
 // Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
+// const oldAnimalNames = (animals) => animals.filter(animal => animal.age > 10).map(animals => animals.name) 
+// console.log(oldAnimalNames(animals))
+
 // Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+// const animalSentence = (animals) => animals.map(animal => `${animal.name} is a ${animal.age} year old ${animal.type}.`)
+// console.log(animalSentence(animals))
+
 // Consider this variable:
 
 const author = {
@@ -62,7 +89,10 @@ const author = {
   genre: "science fiction"
 }
 // Write the code that destructures the author object so that the following code snippet will run successfully:
-// console.log(`${name} is a ${genre} author`)
+const { name } = author;
+const { genre } = author;
+
+console.log(`${name} is a ${genre} author`)
 // // output: "H. G. Wells is a science fiction author"
 // Consider this variable:
 
@@ -76,9 +106,19 @@ const pokeTwo = {
   pokemon_type: "Water"
 }
 // Create a function called describePokemon that take an object like the ones above and uses destructuring to return a description of the Pokemon so that the following code snippet will run successfully:
-// console.log(describePokemon(pokeOne))
+
+
+// const { species, pokemon_type } = pokeTwo
+
+const describePokemon = (obj) => {
+  const { species, pokemon_type } = obj
+  return`${species} is a ${pokemon_type} pokemon.`}
+
+  console.log(describePokemon(pokeOne))
 // // output: "Charmandar is a Fire pokemon"
-// console.log(describePokemon(pokeTwo))
+
+
+console.log(describePokemon(pokeTwo))
 // // output: "Magikarp is a Water pokemon"
 // Consider this variable:
 
@@ -87,7 +127,15 @@ const triangleDimensions = {
   height: 5
 }
 // Modify the triangleDimensions object to have a method that returns the area of the triangle.
+const calculateArea = (triObj) => {
+  return 0.5 * triObj.base * triObj.height
+}
+
+console.log(calculateArea(triangleDimensions))
+
 // Write the code that will update the base to be the value of 6.
+triangleDimensions.base = 6
+console.log(triangleDimensions)
 // 🏔 Stretch Goals
 
 // Consider this variable:
@@ -119,6 +167,7 @@ const triangleDimensions = {
 //   }
 // }
 // Write the code that logs the name of your cohort.
+
 // Write the code that uses destructuring to log the name of your cohort.
 // Create a function that takes an object like the one above and returns an array with a string of every cohort name and year.
 // // output: ["2022 Alpha", "2022 Bravo", "2022 Charlie", "2022 Delta", "2022 Echo", "2022 
