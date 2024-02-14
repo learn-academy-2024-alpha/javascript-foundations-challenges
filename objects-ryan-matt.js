ed68012485020739067258a8a120f7c9b3e74324
Rlemus93 <rlemus93@gmail.com>
2024-02-13 16:54:15 -0800

    finished stretch goals

--- objects-ryan-matt.js
+++ objects-ryan-matt.js
@@ -95,9 +95,17 @@ const animalNames = () => {
 }
 console.log(animalNames(animals));
 // 13.Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
-
-
+const animalAge = (array) => {
+  return array.filter((obj) => obj.age > 10).map((item) => `${item.name}`)
+  }
+ console.log(animalAge(animals))
 //14. Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+const animalSentence = (array) => {
+  return array.map(array => `${array.name} is a good ${array.type} and he is ${array.age} years old.`)
+  
+}
+
+console.log(animalSentence(animals))
 
 
 // Consider this variable:
@@ -107,7 +115,8 @@ const author = {
   genre: "science fiction"
 }
 // 15.Write the code that destructures the author object so that the following code snippet will run successfully:
-// console.log(`${name} is a ${genre} author`)
+const { name, genre } = author 
+console.log(`${name} is a ${genre} author`)
 // // output: "H. G. Wells is a science fiction author"
 
 
@@ -123,9 +132,17 @@ const pokeTwo = {
   pokemon_type: "Water"
 }
 // 16.Create a function called describePokemon that take an object like the ones above and uses destructuring to return a description of the Pokemon so that the following code snippet will run successfully:
-// console.log(describePokemon(pokeOne))
+
+const describePokemon = (arrayOfPoke) => {
+  const { species, pokemon_type } = arrayOfPoke
+  return `${species} is a ${pokemon_type} pokemon`
+}
+
+console.log(describePokemon(pokeOne))
 // // output: "Charmandar is a Fire pokemon"
-// console.log(describePokemon(pokeTwo))
+
+
+console.log(describePokemon(pokeTwo))
 // // output: "Magikarp is a Water pokemon"
 
 
@@ -134,13 +151,18 @@ const pokeTwo = {
 
 const triangleDimensions = {
   base: 2,
-  height: 5
+  height: 5,
+  area: function () {
+    return .5 * this.base * this.height
+  }
 }
 //17. Modify the triangleDimensions object to have a method that returns the area of the triangle.
 
+console.log(triangleDimensions.area())
 
 // 18. Write the code that will update the base to be the value of 6.
-
+triangleDimensions.base = 6
+console.log(triangleDimensions.base)
 
 
 // 🏔 Stretch Goals
@@ -173,11 +195,31 @@ const learn = {
   }
 }
 // 19. Write the code that logs the name of your cohort.
+//console.log(learn.cohorts[2023][0])
+
+console.log(Object.values(learn.cohorts)[1][0])
 
 
 //20.  Write the code that uses destructuring to log the name of your cohort.
 
+const { cohorts: { 2023: cohortNames }  } = learn
+const yourCohortName = cohortNames[0]
+console.log(yourCohortName);
 
 
 // 21. Create a function that takes an object like the one above and returns an array with a string of every cohort name and year.
 // // output: ["2022 Alpha", "2022 Bravo", "2022 Charlie", "2022 Delta", "2022 Echo", "2022 Foxtrot", "2022 Golf", "2022 Hotel", "2023 Alpha", "2023 Bravo", "2023 Charlie", "2023 Delta", "2023 Echo", "2023 Foxtrot", "2023 Golf", "2023 Hotel", "2023 India", "2023 Juliet"]
+
+const cohortStr = (cohortObj) => {
+  let arr = []
+  let yearsObj = cohortObj.cohorts
+  //console.log(yearsObj)
+  for ( const year in yearsObj) {
+    const cohortName = cohortObj.cohorts[year]
+    cohortName.forEach(cohort => {
+      arr.push(`${cohort} ${year}`)
+    })
+  }
+  return arr
+}
+console.log(cohortStr(learn))
