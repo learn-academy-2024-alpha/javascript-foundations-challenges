583e84a8848847d35d0a0b8d8d0261ebd086ddae
sethcrist <sethcrist50@gmail.com>
2024-02-14 11:33:04 -0800

    finished the exercise

--- classes-ryan-seth.js
+++ classes-ryan-seth.js
@@ -57,13 +57,43 @@ latteProfile() {
     return `A ${this.flavor} latte with ${this.milkType} and ${this.numberOfShot} shots of espresso`
 }
 }
-let strongLatte = new Latte("vanilla", "heavy cream", 2)
-console.log(strongLatte.latteProfile())
+// console.log(strongLatte.latteProfile())
+
 // Write the code that makes a regular, single shot latte
+let strongLatte = new Latte("regular", "no milk", 1)
+
 // Log the regular, single shot latte's profile
+console.log(strongLatte.latteProfile());
+
 // Write the code that makes a double shot, hazelnut latte with almond milk.
+strongLatte = new Latte("hazelnut", "almond milk", 2)
 // Log the double shot, hazelnut latte with almond milk's profile.
+console.log(strongLatte.latteProfile());
+
 // Volume of a Cylinder: create a class for Cylinder
+class Cylinder {
+  constructor (radius, height) {
+    this.radius = radius
+    this.height = height
+  }
+
 // Write a class that calculates the volume of a Cylinder: v = πr^2h (r is the radius and h is the height of the cylinder)
+volume () {
+  return Math.PI * this.radius ** 2 * this.height
+}
+}
+let mathVolume = new Cylinder(2, 4)
+console.log(mathVolume.volume());
+
 // Write the code that rounds the volume of the cylinder to four decimal places
+console.log(mathVolume.volume().toFixed(4));
+
 // Write the code that creates three unique cylinder objects
+mathVolume = new Cylinder(4, 8)
+console.log(mathVolume.volume().toFixed(4));
+
+mathVolume = new Cylinder(9, 13)
+console.log(mathVolume.volume().toFixed(4));
+
+mathVolume = new Cylinder(-2, -4)
+console.log(mathVolume.volume().toFixed(4));
